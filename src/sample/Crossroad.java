c5740e81f2fcee00c57f32709c2914eff2bee668
Radekcpp <bielecki02@gmail.com>
2020-06-06 15:34:51 +0200

    little update, added getters and setters

--- src/sample/Crossroad.java
+++ src/sample/Crossroad.java
@@ -4,4 +4,13 @@ public class Crossroad {
     private int crossroad_nr; //number 1-13 defining whichc crossroad it i
 
 
+    public int getCrossroad_nr() {
+        return crossroad_nr;
+    }
+
+    public void setCrossroad_nr(int crossroad_nr) {
+        this.crossroad_nr = crossroad_nr;
+    }
+
+
 }
--- src/sample/TrafficLights.java
+++ src/sample/TrafficLights.java
@@ -1,6 +1,15 @@
 package sample;
 
 public class TrafficLights {
-    private boolean lights_colour; //0-red, 1-green
+    private boolean Lights_colour; //0-red, 1-green
+
+    public void setLights_colour(boolean lights_colour) {
+        this.Lights_colour = lights_colour;
+    }
+
+    public boolean getLights_colour(){
+        return this.Lights_colour;
+    }
+
 
 }
