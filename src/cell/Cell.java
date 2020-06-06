f1bfee831b1f70067c65e467f8e0d30c5d706b74
Ikantek <mczadlo@gmail.com>
2020-06-06 20:53:16 +0200

    Road created

--- src/cell/Cell.java
+++ src/cell/Cell.java
@@ -6,45 +6,48 @@ public class Cell {
     private boolean isCar; // false - empty cell, true - cell with a car (jest na niej auto)
     private RoadType type; // 1 - basic road, 2 - crossroad, 3 - traffic lights
     private CarInstance car;
-    private int speed_limit; //speed limit on this road
-    private int distance_from_lights; //distance from the nearest traffic lights
-    private int next_crossroad; //number 1-13 defining which crossroad is next
+    private int speedLimit; //speed limit on this road
+    private int distanceFromLights; //distance from the nearest traffic lights
+    private int nextCrossroad; //number 0-12 defining which crossroad is next
 
-    public  Cell(){
+    public Cell(){
         this.isCar = false;
     }
 
-    public Cell(RoadType typ) {
+    public Cell(RoadType typeInc, int distance, int nextCrossroadInc) {
         this.isCar = false;
-        this.type=typ;
+        this.speedLimit = 3;
+        setType(typeInc);
+        setDistanceFromLights(distance);
+        setNextCrossroad(nextCrossroadInc);
     }
 
-    public void setType(RoadType type) {
-        this.type = type;
-    }
+    public void setType(RoadType type) { this.type = type; }
 
     public void setCar(boolean ifcar) {
         this.isCar = ifcar;
     }
 
-    public void setDistance_from_lights(int distance_from_lights) {
-        this.distance_from_lights = distance_from_lights;
+    public void setDistanceFromLights(int distance_from_lights) {
+        this.distanceFromLights = distance_from_lights;
     }
 
-    public void setSpeed_limit(int speed_limit) {
-        this.speed_limit = speed_limit;
+    public void setSpeedLimit(int speed_limit) {
+        this.speedLimit = speed_limit;
     }
 
-    public void setNext_crossroad(int next_crossroad) {
-        this.next_crossroad = next_crossroad;
+    public void setNextCrossroad(int nextCrossroad) {
+        if (nextCrossroad == 13)
+            nextCrossroad = 0;
+        this.nextCrossroad = nextCrossroad;
     }
 
-    public int getSpeed_limit() {
-        return speed_limit;
+    public int getSpeedLimit() {
+        return speedLimit;
     }
 
-    public int getDistance_from_lights() {
-        return distance_from_lights;
+    public int getDistanceFromLights() {
+        return distanceFromLights;
     }
 
     public RoadType getType() {
--- src/road/Road.java
+++ src/road/Road.java
@@ -1,15 +1,12 @@
 package road;
-import java.util.Arrays;
-import java.util.stream.IntStream;
 import java.util.Random;
 import cell.Cell;
 import cell.RoadType;
-import java.util.logging.*;
 
 public class Road {
     Cell[][] road1 = new Cell[1733][2];
     Cell[][] road2 = new Cell[1733][2];
-    public static int[]  streetLightPoints = new int[]{0,212,289,471,600,850,982,1174,1282,1407,1457,1493,1563};
+    public static int[] streetLightPoints = new int[]{0,212,289,471,600,850,982,1174,1282,1407,1457,1493,1563,1733};
     public static Weather weather;
     public static DayTime dayTime;
     public int maxCapacity;
@@ -19,6 +16,8 @@ public class Road {
         Random rand = new Random();
         int weatherInt = rand.nextInt(3);
         int dayTimeInt = rand.nextInt(3);
+        int nextLight1 = 1;
+        int nextLight2 = 0;
 
         switch (weatherInt) {
             case 1:
@@ -45,23 +44,34 @@ public class Road {
         }
 
         for (var counter = 0; counter < 1733; counter++) {
-            road1[counter][0] = new Cell(RoadType.Basic);
-            road1[counter][1] = new Cell(RoadType.Basic);
-            road2[counter][0] = new Cell(RoadType.Basic);
-            road2[counter][1] = new Cell(RoadType.Basic);
+            road1[counter][0] = new Cell(RoadType.Basic, streetLightPoints[nextLight1] - counter, nextLight1);
+            road1[counter][1] = new Cell(RoadType.Basic, streetLightPoints[nextLight1] - counter, nextLight1);
+            road2[counter][0] = new Cell(RoadType.Basic, counter - streetLightPoints[nextLight2], nextLight2);
+            road2[counter][1] = new Cell(RoadType.Basic, counter - streetLightPoints[nextLight2], nextLight2);
+
+            if (counter>1281 && counter<1563) {
+                road1[counter][0].setSpeedLimit(2);
+                road1[counter][1].setSpeedLimit(2);
+                road2[counter][0].setSpeedLimit(2);
+                road2[counter][1].setSpeedLimit(2);
+            }
+
+            if (streetLightPoints[nextLight1]==counter)
+                nextLight1++;
+
+            if (streetLightPoints[nextLight2]==counter)
+                nextLight2++;
         }
 
-        for (int x : streetLightPoints) {
-            road1[x][0].setType(RoadType.Lights);
-            road1[x][1].setType(RoadType.Lights);
-            road2[x+2][0].setType(RoadType.Lights);
-            road2[x+2][0].setType(RoadType.Lights);
-            road1[x+1][0].setType(RoadType.Crossroad);
-            road1[x+1][1].setType(RoadType.Crossroad);
-            road2[x+1][0].setType(RoadType.Crossroad);
-            road2[x+1][0].setType(RoadType.Crossroad);
+        for (var counter = 0; counter < (streetLightPoints.length-1); counter++) {
+            road1[streetLightPoints[counter]][0].setType(RoadType.Lights);
+            road1[streetLightPoints[counter]][1].setType(RoadType.Lights);
+            road2[streetLightPoints[counter]+2][0].setType(RoadType.Lights);
+            road2[streetLightPoints[counter]+2][0].setType(RoadType.Lights);
+            road1[streetLightPoints[counter]+1][0].setType(RoadType.Crossroad);
+            road1[streetLightPoints[counter]+1][1].setType(RoadType.Crossroad);
+            road2[streetLightPoints[counter]+1][0].setType(RoadType.Crossroad);
+            road2[streetLightPoints[counter]+1][0].setType(RoadType.Crossroad);
         }
-
-        var counter = 0;
     }
 }
